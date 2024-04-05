c514820b23e88b609c18736c67376a7f997212dd
Peter Strössler <7starch@gmail.com>
2024-04-05 19:33:58 +0200

    test endpoint

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000)
-@app.route('/')
+    app.run(debug = True, host='0.0.0.0',port=5000)
+@app.route('/', methods=['GET'])
 def hello_world():
     return 'Hello, World!'
