ae3b5257ae9297b2cd98e21973c88386f786e00a
Peter Strössler <7starch@gmail.com>
2024-04-05 19:35:01 +0200

    test endpoint

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
-if __name__ == '__main__':
-    app.run(debug = True, host='0.0.0.0',port=5000)
-@app.route('/', methods=['GET'])
+app.config['SECRET_KEY'] = 'mysecretkey'
 def hello_world():
     return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0', port=5000)
