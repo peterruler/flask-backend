f9b41b27cada3c693348e4078e1c62f79064ec73
Peter Strössler <7starch@gmail.com>
2024-04-05 19:25:14 +0200

    test endpoint

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 app = Flask(__name__)
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=4000)
+    app.run(host="127.0.0.1", port=5000)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
