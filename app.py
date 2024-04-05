465ae1dca1c755a17385c0e3a73d15a567649e65
Peter Strössler <7starch@gmail.com>
2024-04-05 19:21:16 +0200

    test endpoint

--- app.py
+++ app.py
@@ -1,33 +1,6 @@
-from flask import Flask, Response, request, jsonify
-from io import BytesIO
-import base64
-from flask_cors import CORS, cross_origin
-import os
-import sys
-
+from flask import Flask
 app = Flask(__name__)
-cors = CORS(app)
-
-if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=4000)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-
-@app.route("/image", methods=['GET', 'POST'])
-def image():
-    if(request.method == "POST"):
-        bytesOfImage = request.get_data()
-        with open('image.jpeg', 'wb') as out:
-            out.write(bytesOfImage)
-        return "Image read"
-
-
-@app.route("/video", methods=['GET', 'POST'])
-def video():
-    if(request.method == "POST"):
-        bytesOfVideo = request.get_data()
-        with open('video.mp4', 'wb') as out:
-            out.write(bytesOfVideo)
-        return "Video read"
